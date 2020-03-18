0488146810ecd5da76491051cc75358b044634e5
Majkel <michal.miszczak13@gmail.com>
2020-03-18 13:22:23 +0100

    Updates

---
+++ precache-manifest.53be4c9d7bbbdda2c850311ee7a16755.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e9116169c1f549189d38",
+    "url": "/cwicz-matme/static/css/main.d69242a0.chunk.css"
+  },
+  {
+    "revision": "e9116169c1f549189d38",
+    "url": "/cwicz-matme/static/js/main.d1a1a0c9.chunk.js"
+  },
+  {
+    "revision": "4d14f50e6436230724c4",
+    "url": "/cwicz-matme/static/js/runtime~main.8119418c.js"
+  },
+  {
+    "revision": "30576794c55eb6a0a3ce",
+    "url": "/cwicz-matme/static/js/2.fc475a00.chunk.js"
+  },
+  {
+    "revision": "63cb5188eabf06491a2e016561186fcd",
+    "url": "/cwicz-matme/index.html"
+  }
+];
